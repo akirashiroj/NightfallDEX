a4e8bae25b2e016a4c24ce42299dc3740cb16fe9
akirashiroj <mmvxxjr262@outlook.com>
2025-11-27 19:13:09 +0800

    deploy setup

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const eEthDeployment = await deploy("ERC7984ETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const eUsdtDeployment = await deploy("ERC7984USDT", {
+    from: deployer,
+    log: true,
+  });
+
+  const swapDeployment = await deploy("NightfallSwap", {
+    from: deployer,
+    args: [eEthDeployment.address, eUsdtDeployment.address],
+    log: true,
+  });
+
+  console.log(`eETH token deployed at ${eEthDeployment.address}`);
+  console.log(`eUSDT token deployed at ${eUsdtDeployment.address}`);
+  console.log(`NightfallSwap contract deployed at ${swapDeployment.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_nightfall_swap"; // id required to prevent reexecution
+func.tags = ["NightfallSwap"];
