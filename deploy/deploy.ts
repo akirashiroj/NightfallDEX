e1071dc00e77438e8642d801105ef194f5d188f2
akirashiroj <mmvxxjr262@outlook.com>
2025-11-27 19:43:59 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,9 +1,12 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+const INITIAL_LIQUIDITY = 10_000n * 1_000_000n;
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+  const { ethers } = hre;
 
   const eEthDeployment = await deploy("ERC7984ETH", {
     from: deployer,
@@ -21,9 +24,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
   });
 
+  const eEthContract = await ethers.getContractAt("ERC7984ETH", eEthDeployment.address);
+  const eUsdtContract = await ethers.getContractAt("ERC7984USDT", eUsdtDeployment.address);
+
+  const seedEthTx = await eEthContract.faucet(swapDeployment.address, INITIAL_LIQUIDITY);
+  await seedEthTx.wait();
+  const seedUsdtTx = await eUsdtContract.faucet(swapDeployment.address, INITIAL_LIQUIDITY);
+  await seedUsdtTx.wait();
+
   console.log(`eETH token deployed at ${eEthDeployment.address}`);
   console.log(`eUSDT token deployed at ${eUsdtDeployment.address}`);
   console.log(`NightfallSwap contract deployed at ${swapDeployment.address}`);
+  console.log(`Seeded NightfallSwap with ${INITIAL_LIQUIDITY.toString()} base units of eETH and eUSDT`);
 };
 export default func;
 func.id = "deploy_nightfall_swap"; // id required to prevent reexecution
